cbd3a3103b6826f7215b1533e0f0fa22ea341731
Christine Wasike <christinewasike22@gmail.com>
2017-09-07 10:39:31 +0300

    Removed commented out code on the constructor in the Animal abstract class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,19 +1,14 @@
 import org.sql2o.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Timestamp;
 
-public abstract class Animal{
+public abstract class Animal {
     public String name;
     public String health;
     public String age;
     public int id;
     public String species;
-
-    // public Animal(String name, String health, String age){
-    //     this.name = name;
-    //     this.health = health;
-    //     this.age = age;
-    // }
 
     public String getName() {
         return name;
@@ -27,39 +22,45 @@ public abstract class Animal{
         return age;
     }
 
-    
-    @Override
-    public boolean equals(Object otherAnimal){
-      if (!(otherAnimal instanceof Animal)) {
-        return false;
-      } else {
-        Animal newAnimal = (Animal) otherAnimal;
-        return this.getName().equals(newAnimal.getName()) &&
-               this.getHealth().equals(newAnimal.getHealth()) &&
-               this.getAge().equals(newAnimal.getAge());
-      }
+    public String getSpecies() {
+        return species;
     }
 
-    public void save() {
-        try(Connection con = DB.sql2o.open()) {
-          String sql = "INSERT INTO animals (name, health, age, species) VALUES (:name, :health, :age, :species)";
-          this.id = (int)
-          con.createQuery(sql, true)
-            .addParameter("name", this.name)
-            .addParameter("health", this.health)
-            .addParameter("age", this.age)   
-            .addParameter("species", species)        
-            .executeUpdate()
-            .getKey();
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName()) && this.getHealth().equals(newAnimal.getHealth())
+                    && this.getAge().equals(newAnimal.getAge());
         }
     }
 
-   
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, health, age, species) VALUES (:name, :health, :age, :species)";
+            this.id = (int) con.createQuery(sql, true).addParameter("name", this.name)
+                    .addParameter("health", this.health).addParameter("age", this.age).addParameter("species", species)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate().getKey();
+        }
+    }
 
     public int getId() {
         return id;
     }
-    
+
+
+
+    public void delete() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE from animals WHERE id = :id";
+            con.createQuery(sql).addParameter("id", id).throwOnMappingFailure(false).executeUpdate();
+            String sql2 = "DELETE from sightings WHERE animal_id = :id";
+            con.createQuery(sql2).addParameter("id", id).throwOnMappingFailure(false).executeUpdate();
+        }
+    }
 
     public static Animal find(int id) {
         try (Connection con = DB.sql2o.open()) {
@@ -68,6 +69,5 @@ public abstract class Animal{
             return animal;
         }
     }
-
 
 }
