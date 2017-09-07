745c4f3a3e7898f8c56b98e7b02ce9e3a1456331
Christine Wasike <christinewasike22@gmail.com>
2017-09-07 10:42:42 +0300

    Thrown exceptions to prevent errors created by users

--- src/main/java/RegularAnimal.java
+++ src/main/java/RegularAnimal.java
@@ -1,21 +1,24 @@
 import org.sql2o.*;
 import java.util.List;
+import java.util.ArrayList;
+import java.sql.Timestamp;
 
 public class RegularAnimal extends Animal {
-    public static final String  DATABASE_SPECIES = "regular";
 
-    public RegularAnimal(String name, String health, String age) {
+    public static final String DATABASE_SPECIES = "regular";
+
+    public RegularAnimal(String name) {
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Please enter an animal name.");
+        }
         this.name = name;
-        this.health = health;
-        this.age = age;
         species = DATABASE_SPECIES;
     }
 
     public static List<RegularAnimal> all() {
         String sql = "SELECT * FROM animals WHERE species='regular';";
         try (Connection con = DB.sql2o.open()) {
-            return con.createQuery(sql)
-            .executeAndFetch(RegularAnimal.class);
+            return con.createQuery(sql).executeAndFetch(RegularAnimal.class);
         }
     }
 }
